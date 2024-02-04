d739338af90d6e3e7205fcceb23d9bee3104ec11
plainp <tarantoj04@gmail.com>
2024-02-04 20:25:12 -0300

    feat: Add Sign In/Out and user menu

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,17 +1,96 @@
+"use client";
+
 import Image from "next/image";
 import { Card, CardContent, CardDescription, CardFooter } from "./ui/card";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
-import { signIn, useSession } from "next-auth/react";
+import {
+  CalendarIcon,
+  HomeIcon,
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+  UserIcon,
+} from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 const Header = () => {
+  const { data } = useSession();
+  const handleLoginClickGoogle = () => {
+    signIn("google");
+  };
+  const handleLogoutClickGoogle = () => {
+    signOut();
+  };
+
   return (
     <Card>
       <CardContent className="p-5 justify-between flex flex-row items-center">
         <Image src="/Logo.png" alt="FSW Barber" height={22} width={120} />
-        <Button variant="outline" size="icon" className="h-8 w-8">
-          <MenuIcon size={18} />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="h-8 w-8">
+              <MenuIcon size={18} />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className="p-0">
+            <SheetHeader className="text-left p-5 border-b border-solid border-secondary">
+              Menu
+            </SheetHeader>
+
+            {data?.user ? (
+              <div className="flex justify-between items-center px-5 py-6">
+                <div className="flex items-center gap-3">
+                  <Avatar>
+                    <AvatarImage src={data.user?.image ?? ""} />
+                  </Avatar>
+
+                  <h2 className="font-bold">{data.user.name}</h2>
+                </div>
+
+                <Button variant="secondary" size="icon">
+                  <LogOutIcon onClick={handleLogoutClickGoogle} />
+                </Button>
+              </div>
+            ) : (
+              <div className="flex flex-col px-5 py-6 gap-5">
+                <div className="flex items-center gap-2">
+                  <UserIcon size={32} />
+                  <h2 className="font-bold">Olá, Faça seu login!</h2>
+                </div>
+                <Button
+                  variant="secondary"
+                  className="w-full justify-start"
+                  onClick={handleLoginClickGoogle}
+                >
+                  <LogInIcon className="mr-3" size={20} />
+                  Fazer Login
+                </Button>
+              </div>
+            )}
+
+            <div className="flex flex-col px-5 gap-3">
+              <Button variant="outline" className="justify-start" asChild>
+                <Link href="/">
+                  <HomeIcon size={18} className="mr-3" />
+                  Início
+                </Link>
+              </Button>
+
+              {data?.user && (
+                <Button variant="outline" className="justify-start" asChild>
+                  <Link href="/agendamentos">
+                    <CalendarIcon size={18} className="mr-3" />
+                    Agendamentos
+                  </Link>
+                </Button>
+              )}
+            </div>
+          </SheetContent>
+        </Sheet>
       </CardContent>
     </Card>
   );
